b97759851459d3fc71af66ca270fe461e9981f00
Matheus <matheustanakanog@gmail.com>
2023-04-26 21:44:49 -0300

    feat: add routes, cors and solved imports

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
+// config imports
 const connectDatabase = require("./config/mongoose.db");
+// routes
+const UserRouter = require("./routes/UserRoutes/userRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(UserRouter);
 
 connectDatabase();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
 });
