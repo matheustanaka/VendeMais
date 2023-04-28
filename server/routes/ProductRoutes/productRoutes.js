5fa3d21aba15b2234f97f35c383559944cbcac8a
Matheus <matheustanakanog@gmail.com>
2023-04-28 12:19:18 -0300

    feat: add controller on routes

--- server/routes/ProductRoutes/productRoutes.js
+++ server/routes/ProductRoutes/productRoutes.js
@@ -1,75 +1,26 @@
 const express = require("express");
 const { requiresAuth } = require("express-openid-connect");
-const ProductModel = require("../../models/Product");
+const {
+  createProduct,
+  getAllProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../../controller/ProductController/ProductController");
 
 const router = express.Router();
 
-// Create a new product
-router.post("/test/products", async (req, res) => {
-  try {
-    const newProduct = new ProductModel(req.body);
+// TO-DO: Needs to add requiresAuth() middleware on all routes.
 
-    await newProduct.save();
+// Create a new product.
+router.post("/products", createProduct);
 
-    res.status(201).json(newProduct);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "It could not create a product", details: error.message });
-  }
-});
+// Get all products of the logged-in user.
+router.get("/products", getAllProduct);
 
-// Get all products of the logged-in user
-router.get("/products", async (req, res) => {
-  try {
-    const products = await ProductModel.find();
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching products" });
-  }
-});
+// Update a product.
+router.put("/products/:id", updateProduct);
 
-// Update a product
-router.put("/products/:id", requiresAuth(), async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const userId = req.session.user._id;
-
-    const updatedProduct = await ProductModel.findOneAndUpdate(
-      { _id: productId, user: userId },
-      req.body,
-      { new: true }
-    );
-
-    if (!updatedProduct) {
-      return res.status(404).json({ error: "Product not found" });
-    }
-
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ error: "Error updating product" });
-  }
-});
-
-// Delete a product
-router.delete("/products/:id", requiresAuth(), async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const userId = req.session.user._id;
-
-    const deletedProduct = await ProductModel.findOneAndDelete({
-      _id: productId,
-      user: userId,
-    });
-
-    if (!deletedProduct) {
-      return res.status(404).json({ error: "Product not found" });
-    }
-
-    res.status(200).json({ message: "Product deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Error deleting product" });
-  }
-});
+// Delete a product.
+router.delete("/products/:id", deleteProduct);
 
 module.exports = router;
