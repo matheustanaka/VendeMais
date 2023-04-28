8b66cf73e33b9066adaf87ccd124cca945dba43d
Matheus <matheustanakanog@gmail.com>
2023-04-27 22:07:50 -0300

    feat: add product router, auth and session config

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const { auth } = require("express-openid-connect");
+const authConfig = require("./config/jwt.auth");
 // config imports
 const connectDatabase = require("./config/mongoose.db");
+const sessionConfig = require("./config/session");
 // routes
 const UserRouter = require("./routes/UserRoutes/userRoutes");
+const ProductRouter = require("./routes/ProductRoutes/productRoutes");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +17,14 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session(sessionConfig));
+
+// Adding auth middleware here
+app.use(auth(authConfig));
+
+// Routes
 app.use(UserRouter);
+app.use("/api", ProductRouter);
 
 connectDatabase();
 
