1382d447b711ef51b39bf58d8fc308935d4da7b3
Matheus Tanaka <matheustanakanog@gmail.com>
2023-05-09 14:44:05 -0300

    fix: add class name to main div

--- web/src/pages/sales/index.tsx
+++ web/src/pages/sales/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Sidebar } from "../../components/Sidebar";
 import "./style.scss";
+import { Table } from "../../components/Table";
 
 export function Sales() {
   return (
-    <div>
+    <div className="dashboard-content">
       <Sidebar />
+      <Table />
     </div>
   );
 }
