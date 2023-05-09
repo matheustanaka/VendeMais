1f124796f8baf5e01976ab48ed503f8dce796a13
Matheus Tanaka <matheustanakanog@gmail.com>
2023-05-09 14:43:44 -0300

    feat: create table component

---
+++ web/src/components/Table/index.jsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+export function Table() {
+  return (
+    <div className="content-section">
+      <main className="main-content">
+        <header>
+          <h1>Dashboard</h1>
+        </header>
+        <section>
+          <p>Conteúdo</p>
+        </section>
+      </main>
+    </div>
+  );
+}
