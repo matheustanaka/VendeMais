a97827e2448c20e4b53b077a271545d5200f2ef6
Matheus <matheustanakanog@gmail.com>
2023-05-14 16:28:57 -0300

    feat: add function to edit modal and import dropdown menu

--- web/src/components/ProductDashboard/index.jsx
+++ web/src/components/ProductDashboard/index.jsx
@@ -1,11 +1,18 @@
+import { Menu, Dropdown, Button } from "antd";
+import { EllipsisOutlined } from "@ant-design/icons";
+
 import { ProductModal } from "../ProductModal";
 import { useProductContext } from "../../hooks/useProductContext";
 import { useState } from "react";
 import "./style.scss";
+import { EditProductModal } from "../EditProductModal";
 
 export function ProductDashboard() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const { products } = useProductContext();
+  const [editModalIsOpen, setEditModalIsOpen] = useState(false);
+  const [currentProductId, setCurrentProductId] = useState(null);
+  const { products, fetchProductById, setName, setDescription, setPrice } =
+    useProductContext();
 
   const openModal = () => {
     setModalIsOpen(true);
@@ -14,6 +21,53 @@ export function ProductDashboard() {
   const closeModal = () => {
     setModalIsOpen(false);
   };
+
+  const EditOpenModal = (productId) => async () => {
+    setCurrentProductId(productId);
+
+    const product = await fetchProductById(productId);
+
+    setName(product.name);
+    setDescription(product.description);
+    setPrice(product.price);
+
+    setEditModalIsOpen(true);
+  };
+
+  const EditCloseModal = () => {
+    setCurrentProductId(null);
+    setEditModalIsOpen(false);
+  };
+
+  // Dropdown
+  const menu = (id) => (
+    <Menu style={{ background: "var(--background)" }}>
+      <Menu.Item className="dropdownItem" key="0">
+        <button
+          style={{
+            color: "white",
+            border: "none",
+            background: "var(--background)",
+          }}
+          onClick={EditOpenModal(id)}
+        >
+          Editar
+        </button>
+      </Menu.Item>
+      <Menu.Item className="dropdownItem" key="1">
+        <button
+          style={{
+            color: "white",
+            border: "none",
+            background: "var(--background)",
+          }}
+          onClick={closeModal}
+        >
+          Deletar
+        </button>
+      </Menu.Item>
+    </Menu>
+  );
 
   return (
     <div className="content-section">
@@ -33,17 +87,30 @@ export function ProductDashboard() {
             <div className="cell">Nome do Produto</div>
             <div className="cell">Descrição</div>
             <div className="cell">Preço</div>
+            <div className="cell"></div>
           </div>
           {products.map((product) => (
             <div className="row" key={product._id}>
               <div className="cell">{product.name}</div>
               <div className="cell">{product.description}</div>
-              <div className="cell">{product.price}</div>
+              <div className="cell">R$ {product.price}</div>
+              <div className="cell">
+                <Dropdown overlay={menu(product._id)}>
+                  <Button className="dropdownButton">
+                    <EllipsisOutlined />
+                  </Button>
+                </Dropdown>
+              </div>
             </div>
           ))}
         </div>
       </section>
       <ProductModal modalIsOpen={modalIsOpen} closeModal={closeModal} />
+      <EditProductModal
+        editModalIsOpen={editModalIsOpen}
+        EditCloseModal={EditCloseModal}
+        productId={currentProductId}
+      />
     </div>
   );
 }
