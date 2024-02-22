865ff447655d408e26dff6cd5e42d48917817a8f
Matheus Tanaka <matheustanakanog@gmail.com>
2024-02-21 21:54:21 -0300

    export globally env vars

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-    server: {
-        port: 8000,
-        host: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        define: {
+                'import.meta.env.REACT_APP_BACKEND_URL': JSON.stringify(env.REACT_APP_BACKEND_URL)
+        },
+        plugins: [react()],
+            server: {
+                port: 8000,
+                host: true,
+        }
     }
 })
+
+
+
